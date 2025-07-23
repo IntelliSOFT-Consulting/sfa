832ca49eac731ad67de54d26cb9b9e8a3f5d38ed
Terry Mochire <terrybmochire@gmail.com>
2025-07-23 07:13:35 +0300

    Update token expiry to 3 hours.

--- api/auth_module/auth.py
+++ api/auth_module/auth.py
@@ -43,7 +43,7 @@ def authenticate_user(username: str, password: str):
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    expire = datetime.utcnow() + (expires_delta or timedelta(hours=3))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
